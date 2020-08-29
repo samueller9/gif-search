50ef7673dd4f13478c9fb6c54dcfdfdb188747b4
samueller9 <60405236+samueller9@users.noreply.github.com>
2020-08-29 14:51:53 -0700

    added working gif search

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 // Require Libraries
 const express = require('express');
+// Require tenorjs near the top of the file
+const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": "AGEE7SPER7CS", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+
 
 // App Setup
 const app = express();
@@ -28,6 +36,22 @@ app.get('/greetings/:name', (req, res) => {
   // render the greetings view, passing along the name
   res.render('greetings', { name });
 })
+
+app.get('/', (req, res) => {
+    // Handle the home page when we haven't queried yet
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    Tenor.Search.Query(term, "10")
+        .then(response => {
+            // store the gifs we get back from the search
+            const gifs = response;
+            // pass the gifs as an object into the home page
+            res.render('home', { gifs })
+        }).catch(console.error);
+  })
 
 // Start Server
 
